c422afe558e0aa44770139f8231bfc8b96116cb7
Sappakit <pipitwattana.s@gmail.com>
2024-10-30 13:29:48 +0700

    feat: add external navigation, functional tags, and link copy button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,45 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import { Link } from "lucide-react";
+
 import "./App.css";
 
-function BlogPosts({ blogList }) {
+function BlogPosts({ blogList, setInputChange }) {
   return (
     <section className="flex flex-col items-start gap-12">
       {blogList.map((blog) => (
-        <div className="flex items-center justify-center gap-9" key={blog.eid}>
-          <img
-            className="h-[15rem] w-[22.5rem] rounded-3xl object-cover"
-            src={blog.photos[0]}
-            alt=""
-          />
+        <div
+          className="flex w-full items-center justify-center gap-9"
+          key={blog.eid}
+        >
+          {/* Blog image */}
+          <a href={blog.url}>
+            <img
+              className="h-[15rem] w-[22.5rem] rounded-3xl object-cover"
+              src={blog.photos[0]}
+              alt=""
+            />
+          </a>
 
-          <div className="flex flex-col gap-4 text-neutral-500">
+          {/* Content */}
+          <div className="relative flex w-[60%] flex-col gap-4 text-neutral-500">
+            {/* Copy link */}
+            <div
+              className="absolute bottom-0 right-5 flex size-14 cursor-pointer items-center justify-center rounded-full border-[3px] border-sky-500"
+              onClick={() => {
+                navigator.clipboard.writeText(blog.url);
+              }}
+            >
+              <Link className="size-8 text-sky-500" />
+            </div>
+
             <div>
-              <h2 className="text-2xl font-semibold text-neutral-950">
-                {blog.title}
-              </h2>
+              <a href={blog.url}>
+                <h2 className="inline text-2xl font-semibold text-neutral-950">
+                  {blog.title}
+                </h2>
+              </a>
               <p>
                 {blog.description.length > 100
                   ? `${blog.description.slice(0, 100)} ...`
@@ -34,7 +55,12 @@ function BlogPosts({ blogList }) {
                   <>
                     {index === blog.tags.length - 1 && <p>และ</p>}
 
-                    <a className="underline" href="" key={index}>
+                    <a
+                      className="underline"
+                      href="#"
+                      key={index}
+                      onClick={() => setInputChange(tag)}
+                    >
                       {tag}
                     </a>
                   </>
@@ -44,12 +70,14 @@ function BlogPosts({ blogList }) {
 
             <figure className="flex gap-7">
               {blog.photos.slice(1, 4).map((photo, index) => (
-                <img
-                  className="size-24 rounded-lg object-cover"
-                  src={photo}
-                  alt=""
-                  key={index}
-                />
+                <a href={blog.url}>
+                  <img
+                    className="size-24 rounded-lg object-cover"
+                    src={photo}
+                    alt=""
+                    key={index}
+                  />
+                </a>
               ))}
             </figure>
           </div>
@@ -69,18 +97,18 @@ function App() {
         `http://localhost:4001/trips?keywords=${inputChange}`,
       );
 
-      // log
-      console.log(response.data.data);
-
       setBlogList([...response.data.data]);
     } catch (error) {
       console.log(error);
-    } finally {
     }
   };
 
   useEffect(() => {
-    fetchData();
+    const fetchTimeout = setTimeout(() => {
+      fetchData();
+    }, 500);
+
+    return () => clearTimeout(fetchTimeout);
   }, [inputChange]);
 
   return (
@@ -93,16 +121,17 @@ function App() {
           <div className="flex w-full flex-col gap-1 px-32">
             <p className="text-neutral-950">ค้นหาที่เที่ยว</p>
             <input
-              className="w-full border-b-2 placeholder:text-center"
+              className="w-full border-b-2 p-1 text-center placeholder:text-center focus:outline-none"
               type="text"
               placeholder="หาที่เที่ยวแล้วไปกัน ..."
+              value={inputChange}
               onChange={(e) => setInputChange(e.target.value)}
             />
           </div>
         </div>
 
         {/* Blog posts */}
-        <BlogPosts blogList={blogList} />
+        <BlogPosts blogList={blogList} setInputChange={setInputChange} />
       </section>
     </div>
   );
